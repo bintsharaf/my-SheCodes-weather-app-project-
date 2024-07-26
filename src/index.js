c2fab0eacd2dfd4ce9aa8dd13964206f6c9ca196
bintsharaf <138716264+bintsharaf@users.noreply.github.com>
2024-07-26 16:27:04 +0100

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,12 +2,33 @@ function updateWeatherInfo(response) {
     
     let temperatureElement = document.querySelector("#temp-number");
     let temperature = Math.round(response.data.temperature.current);
-    console.log(response.data.temperature.current);
-    temperatureElement.innerHTML = temperature;
     let searchCityElement = document.querySelector("#Search-input-city");
-    searchCityElement.innerHTML = response.data.city;
+    let weatherDescriptionElement = document.querySelector("#weather-description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind"); 
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);  
+    console.log(date);
 
+    temperatureElement.innerHTML = temperature;
+    searchCityElement.innerHTML = response.data.city;
+    weatherDescriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${response.data.wind.speed}km/h`;
+    //created a function to display the real day 
+    timeElement.innerHTML = formartDate(date);
 }
+function formartDate(date) {
+    
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+    let day = days[date.getDay()];
+        if (minutes < 10) {
+            minutes = `0${minutes}`;
+        }
+    return `${day} ${hours}:${minutes}`
+}   
 function linkCity(city) {
     let  apiKey = "29583e5b03o3adtc2486edaf9f3af0e3"
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
@@ -20,6 +41,7 @@ function onClickForm(event) {
     
     linkCity(searchInputElement.value);
 }
+
 
 let formElement = document.querySelector("#Search-form");
 formElement.addEventListener("submit", onClickForm); 
